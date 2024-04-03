17f2a6cf3998ae957aaed5d5ae970accda0b7f51
Hannah <hannah06@uw.edu>
2024-04-03 16:27:20 -0700

    added new file selectors; updating styles

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-"use strict";
-
-(function() {
+// import { appendFile } from 'node:fs';
+// import addToFile from './test';
 
 const STOP_WORDS_LINK = "stop_words.txt";
 
 let stopWordsList = [];
 
-let currTokensList = new Set();
-let setIter = currTokensList[Symbol.iterator]();
-// let currToken = 0;
+const AFINN_LEXICON_LINK = "afinn_lexicon-en-165.txt";
+const LEXICON_LINK = "lexicon.txt";
+
+let AFINN_obj = {};
+let customLexiconObj = {};
+let fullLexicon = {};
+
+let currTokensList
+let currToken = 0;
+
+let tokensToAddToStopWords = [];
+let tokensToAddToLexicon = [];
 
 
 // when the page loads, call "init"
@@ -19,30 +27,63 @@ window.addEventListener("load", init);
 function init() {
 
     getFileStopWords();
+    getFileLexicons(); // creates global JSON lexicon objects for the AFINN and historical lexicons
 
     // hiding things that shouldn't be visible when the page loads
-    id("buttonsSection").style.display = "none";
+    id("scoringSection").style.display = "none";
+    id("listsSection").style.display = "none";
 
     // adding general event listeners (options section button, main text input)
     id("addButton").addEventListener("click", addButtonClick);
     id("ignoreButton").addEventListener("click", ignoreButtonClick);
-    // id("input").addEventListener("input", processInputtedText);
-    id("inputFile").addEventListener("change", processInputtedFile);
+    id("inputTextFile").addEventListener("change", processInputtedTextFile);
+    // probably goign to need to add a processFileInput() event listener to each of three file inputs,
+    // in that, check to see if all three have a value
+    // if so, then do the last part
+    // or - could add a go button, that's disabled until all three things have a file selected
 
 }
 
 function addButtonClick() {
-    // currToken = currToken + 1;
-    console.log("add button clicked!");
-    id("token").textContent = setIter.next().value;
+    // add to lexicon
+    let token = currTokensList[currToken];
+    fullLexicon[token] = 6;
+    tokensToAddToLexicon.push(token);
+    let line = token + " 6"
+    if (id("listsSection").style.display == "none") {
+        id("listsSection").style.display = "flex"
+    }
+    appendTextElementToSection('p', id("newLexiconTokens"), line)
+
+    displayNextToken();
+    // appendFile('lexicon.txt', line, (err) => {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         console.log(line + " has been successfully appended to the lexicon");
+    //     }
+    // })
+    // addToFile('lexicon.txt', line);
 }
 
 function ignoreButtonClick() {
-    // currToken = currToken + 1;
-    console.log("ingore button clicked!");
-    id("token").textContent = setIter.next().value;
+    // add to stopwords list
+    let token = currTokensList[currToken];
+    stopWordsList.push(token);
+    tokensToAddToStopWords.push(token);
+    // this section is hidden until at least one word is added to one of the sections
+    if (id("listsSection").style.display == "none") {
+        id("listsSection").style.display = "flex"
+    }
+    appendTextElementToSection('p', id("newStopWords"), token);
+
+    displayNextToken();
 }
 
+function displayNextToken() {
+    currToken = currToken + 1;
+    id("token").textContent = currTokensList[currToken];
+}
 
 
 
@@ -61,17 +102,42 @@ function processFileStopWords(list, resultList) {
 
 
 
+async function getFileLexicons() {
+// call this once when page loads; make JSON lexicons for both, then save those to variables
+// then use another function to decide which of the lexicon vars to use
+    Promise.all([
+        fetch(AFINN_LEXICON_LINK).then(x => x.text()),
+        fetch(LEXICON_LINK).then(x => x.text())
+    ]).then(([afinnLexicon, customLexicon]) => {
+        processFileLexicon(afinnLexicon, '\t', AFINN_obj);
+        processFileLexicon(customLexicon, ' ', customLexiconObj);
+        fullLexicon = {...AFINN_obj, ...customLexiconObj};
+    });
+}
+
+function processFileLexicon(lexicon, separator, lexiconObj) {
+    let lines = lexicon.split('\n');
+    lines.forEach((line) => {
+        let [word, score] = line.split(separator);
+
+        lexiconObj[word] = score;
+    })
+}
 
 
 
 
 
-function processInputtedFile() {
-    id("buttonsSection").style.display = "flex"; // unhide the buttons
+
+
+
+function processInputtedTextFile() {
+    id("scoringSection").style.display = "flex"; // unhide the buttons
     let fr = new FileReader();
     fr.onload = function() {
         let file_text = fr.result;
         processText(file_text);
+        // might need to add something here to update lexicons and stopwords lists
     }
     for (let i = 0; i < this.files.length; i++) {
         let currFile = this.files[i];
@@ -80,27 +146,25 @@ function processInputtedFile() {
 }
 
 function processText(text) {
-    currTokensList = currTokensList.clear();
+    currTokensList = [];
+
     let formatted_text = lowerCaseAndRemovePunctuationOfText(text);
     let tokens = tokenizeText(formatted_text);
-    // console.log(tokens)
-    let filteredTokens = removeStopWords(tokens, stopWordsList);
-    // console.log(stopWordsList)
-    // console.log(filteredTokens)
 
-    // filteredTokens.forEach((token) => {
-    //     currTokensList.add(token);
-    // })
+    // we don't want to show any words already in lexicon or stop words lists
+    let tokensSansStopWords = filterTokens(tokens, stopWordsList);
+    let tokensSansLexiconTerms = filterTokens(tokensSansStopWords, Object.keys(fullLexicon));
 
-    for (let i = 0; i < filteredTokens.length; i++) {
-        let item = filteredTokens[i];
-        currTokensList.add(item);
+    // removing duplicate words from the words we'll show to users
+    for (let i = 0; i < tokensSansLexiconTerms.length; i++) {
+        let token = tokensSansLexiconTerms[i];
+        if (!currTokensList.includes(token)) {
+            currTokensList.push(token);
+        }
     }
+    currToken = 0;
 
-    // currTokensList = filteredTokens;
-    // currToken = 0;
-    id("token").textContent = setIter.next().value; //currTokensList[currToken];
-
+    id("token").textContent = currTokensList[currToken];
 }
 
 
@@ -123,9 +187,9 @@ function tokenizeText(text) {
 }
 
 
-function removeStopWords(tokens, stopWordsList) {
-    let filteredTokens = tokens.filter((token) => !stopWordsList.includes(token))
-    return filteredTokens
+function filterTokens(tokens, listOfTokensToFilterOut) {
+    let filteredTokens = tokens.filter((token) => !listOfTokensToFilterOut.includes(token));
+    return filteredTokens;
 }
 
 
@@ -162,5 +226,3 @@ function id(idName) {
 function gen(tagName) {
     return document.createElement(tagName);
 }
-
-})();
